c5fc915876ce4fc1fe8b4bc159b20e607c9a8d8e
Integralsia <akvegetacocatrada3@gmail.com>
2026-03-07 02:23:26 +0700

    a

--- lab 1 -  Abstract Factory/CarMaker/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ lab 1 -  Abstract Factory/CarMaker/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -94,7 +94,7 @@ namespace CarMaker {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CarMaker;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CarMaker;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
---
+++ lab 2 - Flyweight/Undertale/Attacks/Attacks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Undertale.Attacks
+{
+    internal class Attacks
+    {
+         
+    }
+
+    public class Bone : Attacks
+    {
+        private int width;
+        private int height;
+    
+    }
+}
