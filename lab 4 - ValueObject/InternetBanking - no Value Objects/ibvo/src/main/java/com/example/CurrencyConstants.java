619e59aa80f92371af0fad5516a1fe7f33fbfef8
solovozer <akvegetacocatrada3@gmail.com>
2026-05-08 12:47:33 +0700

    a

--- lab 4 - ValueObject/InternetBanking - no Value Objects/ibvo/src/main/java/com/example/CurrencyConstants.java
+++ lab 4 - ValueObject/InternetBanking - no Value Objects/ibvo/src/main/java/com/example/CurrencyConstants.java
@@ -4,5 +4,5 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CurrencyConstants {
-    public static final List<String> SUPPORTED_CURRENCIES = Arrays.asList("AUD", "CAD", "CNY", "EUR", "JPY", "RUB", "USD", "VND");
+    public static final List<String> SUPPORTED_CURRENCIES = Arrays.asList("VND", "USD", "RUB", "CNY", "EUR", "AUD", "JPY", "CAD");
 }
