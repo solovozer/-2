fc39f03398f9355be435c5f85a7b92b4d504dc8e
solovozer <123140848+solovozer@users.noreply.github.com>
2026-07-07 13:28:09 +0700

    Delete lab 4 - ValueObject directory

--- lab 4 - ValueObject/InternetBanking - Value Objects/ibvo/src/main/java/com/example/CurrencyConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class CurrencyConstants {
-    public static final List<String> SUPPORTED_CURRENCIES = Arrays.asList("AUD", "CAD", "CNY", "EUR", "JPY", "RUB", "USD", "VND");
-}
--- lab 4 - ValueObject/InternetBanking - no Value Objects/ibvo/src/main/java/com/example/CurrencyConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class CurrencyConstants {
-    public static final List<String> SUPPORTED_CURRENCIES = Arrays.asList("VND", "USD", "RUB", "CNY", "EUR", "AUD", "JPY", "CAD");
-}
